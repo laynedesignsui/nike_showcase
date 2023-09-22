c4025362cfca051457ea9a0cb2c27940a64316fe
laynedesignsui <112714618+laynedesignsui@users.noreply.github.com>
2023-09-22 11:43:57 -0400

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,34 @@
+import { headerLogo } from "../assets/images";
+
+import { hamburger } from "../assets/icons";
+
+import { navLinks } from "../../constants";
+
+const Nav = () => {
+  return (
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={130} height={29} />
+        </a>
+        <ul className="flex-1 flex -translate-x-8 justify-end items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="fonts-monts errat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="lg:hidden">
+            <img src={hamburger} alt="" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
---
+++ src/sections/index.js
@@ -0,0 +1,21 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQaulity from "./SuperQaulity";
+import Nav from "../components/Nav";
+
+export {
+    Hero,
+    CustomerReviews,
+    Footer,
+    PopularProducts,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQaulity,
+    Nav
+}
